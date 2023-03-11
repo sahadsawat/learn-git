8b168f10b1f92b48632df733fe997b4ad7bd844a
sahadsawat <mr.fapman_nongnine@hotmail.com>
2023-03-11 23:12:57 +0700

    new changes2

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           appBar: AppBar(
             title: Text('ล็อกอินฟอร์ม101'),
+            shadowColor: Colors.amber,
           ),
           body: Padding(
             padding: const EdgeInsets.all(20.0),
